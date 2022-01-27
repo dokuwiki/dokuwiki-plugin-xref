d4a11906f9835a31d350a4bd6d056ff687a71365
Andreas Gohr <andi@splitbrain.org>
2022-01-27 23:12:27 +0100

    new mechanisms to reference Grok instead of phprxref
    This implements a mechanism that transforms our current references into
    search terms understood by Grok. Using the API we can even check if
    there's any hits (ideally it should be exactly one)

---
+++ Grok.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace dokuwiki\plugin\xref;
+
+use dokuwiki\HTTP\DokuHTTPClient;
+
+class Grok
+{
+
+    protected $baseUrl;
+    protected $def;
+    protected $path;
+
+    public function __construct($reference, $baseUrl = 'https://codesearch.dokuwiki.org')
+    {
+        $heuristic = new Heuristics($reference);
+        $this->def = $heuristic->getDef();
+        $this->path = $heuristic->getPath();
+        $this->baseUrl = rtrim($baseUrl, '/');
+    }
+
+    /**
+     * Return the URL that leads to the search interface
+     *
+     * @return string
+     */
+    public function getSearchUrl()
+    {
+        $url = $this->baseUrl . '/search?';
+        $param = [
+            'project' => 'dokuwiki',
+            'defs' => $this->def,
+            'path' => $this->path,
+        ];
+        $url .= buildURLparams($param, '&');
+        return $url;
+    }
+
+    /**
+     * Return the URL that allows to query the API
+     *
+     * @return string
+     */
+    public function getAPIUrl()
+    {
+        $url = $this->baseUrl . '/api/v1/search?';
+        $param = [
+            'projects' => 'dokuwiki',
+            'def' => $this->def,
+            'path' => $this->path,
+        ];
+        $url .= buildURLparams($param, '&');
+        return $url;
+    }
+
+    /**
+     * Return the number of results to expect
+     *
+     * @return false|int false on errors
+     */
+    public function getResultCount()
+    {
+        $http = new DokuHTTPClient();
+        $http->timeout = 5;
+        $json = $http->get($this->getAPIUrl());
+        if (!$json) return false;
+        $data = json_decode($json, true);
+        if (!$data) return false;
+        if (!isset($data['resultCount'])) return false;
+        return $data['resultCount'];
+    }
+}
